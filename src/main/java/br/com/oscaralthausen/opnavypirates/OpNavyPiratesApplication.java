3e8dd606cc3fddde5adba874962731ec5c778711
lighthauzen <oscar.casavelha@gmail.com>
2023-08-15 18:14:26 -0300

    first commit

---
+++ src/main/java/br/com/oscaralthausen/opnavypirates/OpNavyPiratesApplication.java
@@ -0,0 +1,13 @@
+package br.com.oscaralthausen.opnavypirates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpNavyPiratesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpNavyPiratesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/oscaralthausen/opnavypirates/OpNavyPiratesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oscaralthausen.opnavypirates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpNavyPiratesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
